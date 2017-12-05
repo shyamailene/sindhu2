fdddd4cdac87862556a19a893d1f106335b3a7ec
Ailene <shyam.ailene@hpe.com>
2017-12-05 10:52:40 +0530

    trac changes commit

---
+++ src/main/webapp/app/dashboard/dashboard.state.js
@@ -0,0 +1,32 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('sindhuApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider.state('dashboard', {
+            parent: 'app',
+            url: '/',
+            data: {
+                authorities: []
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/dashboard/dashboard.html',
+                    controller: 'DashboardController',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
+                    $translatePartialLoader.addPart('dashboard');
+                    return $translate.refresh();
+                }]
+            }
+        });
+    }
+})();
